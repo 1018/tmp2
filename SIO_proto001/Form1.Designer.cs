d776fe9205c485350a5e584b7fb10bacf49d9dea
unknown <yasuda@yasuda.ksskyt.local>
2012-07-09 17:12:53 +0900

    2nd commit

--- SIO_proto001/Form1.Designer.cs
+++ SIO_proto001/Form1.Designer.cs
@@ -33,12 +33,12 @@
             this.inputFilePath = new System.Windows.Forms.TextBox();
             this.outputFilePath = new System.Windows.Forms.TextBox();
             this.trashMode = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.ExtractCp = new System.Windows.Forms.CheckBox();
+            this.ExtractHp = new System.Windows.Forms.CheckBox();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.convertBtn = new System.Windows.Forms.Button();
             this.selectBtn = new System.Windows.Forms.Button();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -63,46 +63,46 @@
             // 
             this.inputFilePath.Location = new System.Drawing.Point(72, 24);
             this.inputFilePath.Name = "inputFilePath";
-            this.inputFilePath.Size = new System.Drawing.Size(424, 19);
+            this.inputFilePath.Size = new System.Drawing.Size(336, 19);
             this.inputFilePath.TabIndex = 2;
             // 
             // outputFilePath
             // 
             this.outputFilePath.Location = new System.Drawing.Point(72, 48);
             this.outputFilePath.Name = "outputFilePath";
-            this.outputFilePath.Size = new System.Drawing.Size(424, 19);
+            this.outputFilePath.Size = new System.Drawing.Size(336, 19);
             this.outputFilePath.TabIndex = 3;
-            this.outputFilePath.Text = "D:\\nagase\\WorkSpace\\SIO_proto001\\SIO.txt";
+            this.outputFilePath.Text = "..\\..\\..\\SIO.txt";
             // 
             // trashMode
             // 
             this.trashMode.AutoSize = true;
             this.trashMode.Location = new System.Drawing.Point(24, 80);
             this.trashMode.Name = "trashMode";
-            this.trashMode.Size = new System.Drawing.Size(101, 16);
+            this.trashMode.Size = new System.Drawing.Size(84, 16);
             this.trashMode.TabIndex = 4;
-            this.trashMode.Text = "いらんやつ捨てる";
+            this.trashMode.Text = "ごみ箱もーど";
             this.trashMode.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // ExtractCp
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(144, 80);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(84, 16);
-            this.checkBox2.TabIndex = 5;
-            this.checkBox2.Text = "CPのみ抽出";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.ExtractCp.AutoSize = true;
+            this.ExtractCp.Location = new System.Drawing.Point(144, 80);
+            this.ExtractCp.Name = "ExtractCp";
+            this.ExtractCp.Size = new System.Drawing.Size(84, 16);
+            this.ExtractCp.TabIndex = 5;
+            this.ExtractCp.Text = "CPのみ抽出";
+            this.ExtractCp.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // ExtractHp
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(144, 104);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(84, 16);
-            this.checkBox3.TabIndex = 6;
-            this.checkBox3.Text = "HPのみ抽出";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.ExtractHp.AutoSize = true;
+            this.ExtractHp.Location = new System.Drawing.Point(144, 104);
+            this.ExtractHp.Name = "ExtractHp";
+            this.ExtractHp.Size = new System.Drawing.Size(84, 16);
+            this.ExtractHp.TabIndex = 6;
+            this.ExtractHp.Text = "HPのみ抽出";
+            this.ExtractHp.UseVisualStyleBackColor = true;
             // 
             // checkBox4
             // 
@@ -116,7 +116,7 @@
             // 
             // convertBtn
             // 
-            this.convertBtn.Location = new System.Drawing.Point(432, 80);
+            this.convertBtn.Location = new System.Drawing.Point(416, 64);
             this.convertBtn.Name = "convertBtn";
             this.convertBtn.Size = new System.Drawing.Size(64, 32);
             this.convertBtn.TabIndex = 8;
@@ -126,7 +126,7 @@
             // 
             // selectBtn
             // 
-            this.selectBtn.Location = new System.Drawing.Point(512, 24);
+            this.selectBtn.Location = new System.Drawing.Point(416, 24);
             this.selectBtn.Name = "selectBtn";
             this.selectBtn.Size = new System.Drawing.Size(64, 32);
             this.selectBtn.TabIndex = 9;
@@ -134,35 +134,35 @@
             this.selectBtn.UseVisualStyleBackColor = true;
             this.selectBtn.Click += new System.EventHandler(this.selectBtn_Click);
             // 
-            // checkBox5
+            // checkBox1
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(240, 104);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(188, 16);
-            this.checkBox5.TabIndex = 10;
-            this.checkBox5.Text = "処理終了後変換ﾌｧｲﾙを表示する";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(240, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "ﾉｰﾄﾞ2のみ抽出";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(602, 138);
-            this.Controls.Add(this.checkBox5);
+            this.ClientSize = new System.Drawing.Size(502, 138);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.selectBtn);
             this.Controls.Add(this.convertBtn);
             this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.ExtractHp);
+            this.Controls.Add(this.ExtractCp);
             this.Controls.Add(this.trashMode);
             this.Controls.Add(this.outputFilePath);
             this.Controls.Add(this.inputFilePath);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SIOﾛｸﾞ変換ﾂｰﾙ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,12 +175,12 @@
         private System.Windows.Forms.TextBox inputFilePath;
         private System.Windows.Forms.TextBox outputFilePath;
         private System.Windows.Forms.CheckBox trashMode;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox ExtractCp;
+        private System.Windows.Forms.CheckBox ExtractHp;
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.Button convertBtn;
         private System.Windows.Forms.Button selectBtn;
-        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
